af39b26392b586bcdb113d433727a8b625fe4004
Robert Brackett <robert.brackett3@gmail.com>
2010-12-03 14:41:13 -0500

    fixed pagination on feed items

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User
                                 :index => true
   references_many   :comments,  :stored_as => :array,
                                 :inverse_of => :user
+                                :index => true
   references_many   :following, :stored_as => :array,
                                 :class_name => 'User',
                                 :inverse_of => :followers
