0c3c9ba158e1cdcae29b66b619d66d533dbaf496
Robert Brackett <robert.brackett3@gmail.com>
2010-12-06 17:08:13 -0500

    go aws go

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,13 @@
-if Rails.env.production?
-  AWS::S3::Base.establish_connection!(
-    :access_key_id      => ENV['S3_KEY'],
-    :secret_access_key  => ENV['S3_SECRET'],
-    :bucket             => ENV['S3_BUCKET']
-  )
-end
+require 'yaml'
+require 'rubygems'
+require 'aws/s3'
+
+AWS::S3::Base.establish_connection!(
+  :access_key_id      => ENV['S3_KEY'],
+  :secret_access_key  => ENV['S3_SECRET']
+)
+
+AWS::S3::Service.buckets(
+  :bucket             => ENV['S3_BUCKET']
+)
 
