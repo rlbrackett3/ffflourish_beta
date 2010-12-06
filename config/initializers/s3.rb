21ea7570e25058974a6a750d17ca483b00ea61bd
Robert Brackett <robert.brackett3@gmail.com>
2010-12-06 16:47:19 -0500

    conditional load AWS ENV in development

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,8 @@
-AWS::S3::Base.establish_connection!(
-  :access_key_id      => ENV['S3_KEY'],
-  :secret_access_key  => ENV['S3_SECRET']
-  :s3_bucket          => ENV['S3_BUCKET']
-)
+if Rails.env.production?
+  AWS::S3::Base.establish_connection!(
+    :access_key_id      => ENV['S3_KEY'],
+    :secret_access_key  => ENV['S3_SECRET'],
+    :bucket             => ENV['S3_BUCKET']
+  )
+end
 
