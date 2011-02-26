ab5be9b54b733a2a6a9ced259d46d105f2ddeda2
Robert Lee Brackett <robert.brackett3@gmail.com>
2011-02-25 22:15:16 -0500

    updated popular feed to only shopw posts within one day

--- app/models/post.rb
+++ app/models/post.rb
@@ -50,7 +50,7 @@ class Post
 
   scope :recent, where(:created_at.gt => 5.days.ago).desc(:created_at)#write tests for me
 
-  scope :popular, order_by(:votes.desc)#write tests for me
+  scope :popular, where(:created_at.gt => 1.day.ago).order_by(:votes.desc)#write tests for me
   
   scope :commented_on_by_user, lambda { |user| where( { "comments.user_id" => user.id } ).desc(:updated_at) }
 
